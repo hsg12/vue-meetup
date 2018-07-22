9be96e874016d85688c40dd1a0317e0a9807d442
php-user1 <test@test.com>
2018-07-22 17:04:09 +0400

    Start

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app temporary v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon left>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>View Meetups</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="purple darken-4">
+      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>MeetUp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false
+    }
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+  
+</style>
